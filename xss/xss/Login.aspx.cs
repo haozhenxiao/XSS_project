c07d7175a8312eb47cacb5453df4b608ddc4f78f
haozhenxiao <haozhenxiao2010@gmail.com>
2014-05-04 13:05:20 +0200

    database connection has set up

--- xss/xss/Login.aspx.cs
+++ xss/xss/Login.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
 
 public partial class Login : System.Web.UI.Page
 {
@@ -14,6 +17,33 @@ public partial class Login : System.Web.UI.Page
 
     void Login_Click(Object sender,
                            EventArgs e) {
-        Response.Redirect("Welcome.aspx");
+        
+        SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DbConnection"].ToString());       
+
+        string username = TextBox1.Text;
+        string password = TextBox2.Text;
+        string sql = ("SELECT * FROM User WHERE name='" + username + "' AND password='" + password + "';");
+        SqlCommand command = new SqlCommand();
+        command.CommandText = "SELECT * FROM User;";
+        command.CommandType = CommandType.Text;
+        command.Connection = connection;
+        
+        SqlDataReader reader = null;
+        try {
+            connection.Open();
+           // reader = command.ExecuteReader();
+            //if (!reader.Equals(null))
+            //{
+            //    Response.Redirect("Welcome.aspx");
+            //}
+            TextBox1.Text = "OK";
+        }
+        catch (SqlException ex) {
+            TextBox1.Text = "NOT OK";
+            Console.WriteLine("00000000000 "+ex.Message);
+        }
+        
+        connection.Close();
+        
     }
 }
